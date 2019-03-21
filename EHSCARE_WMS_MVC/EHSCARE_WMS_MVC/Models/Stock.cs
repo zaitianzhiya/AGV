444d7e5e1684e2ae69c3512bd49df024afccda48
AGVProject <42537024+shuicheng9@users.noreply.github.com>
2019-03-21 09:03:07 +0800

    Create Stock.cs

---
+++ EHSCARE_WMS_MVC/EHSCARE_WMS_MVC/Models/Stock.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EHSCARE_WMS.Models
+{
+    public class Stock
+    {
+        public string BOX_NO { get; set; }
+        public string BOX_BARCODE { get; set; }
+        public string STORE_NO { get; set; }
+        public string STORE_NAME { get; set; }
+        public string WH_NO { get; set; }
+        public string WH_NAME { get; set; }
+        public int WH_IS_USED { get; set; }
+        public string WH_IS_USED_NAME { get; set; }
+        public string FORBBIDEN_REASON { get; set; }
+        public DateTime CREATE_TIME { get; set; }
+        public string CREATE_USERID { get; set; }
+        public string CREATE_USERNAME { get; set; }
+        public DateTime UPDATE_TIME { get; set; }
+        public string UPDATE_USERID { get; set; }
+        public string UPDATE_USERNAME { get; set; }
+        public int DEMOSUM { get; set; }
+
+    }
+}
